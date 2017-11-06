64b3a36bc6d94e55759de8309f898f47ced14cae
Zhujieru <chamber_of_secrets@163.com>
2017-11-06 09:34:36 +0800

    新建ActionBarView,作为整个app的actionbar

--- app/src/main/java/com/fragment/MyInfoFragment.java
+++ app/src/main/java/com/fragment/MyInfoFragment.java
@@ -1,9 +1,11 @@
 package com.fragment;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by zjr on 2017/10/31.
  * "我的"对应的fragmen
  */
 
-public class MyInfoFragment {
+public class MyInfoFragment extends Fragment{
 }
---
+++ app/src/main/java/com/view/ActionBarView.java
@@ -0,0 +1,51 @@
+package com.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.user.crmapp.R;
+
+/**
+ * Created by zjr on 2017/11/6.
+ * 自定义的view,作为整个app的actionbar
+ */
+
+public class ActionBarView extends LinearLayout {
+
+
+    private ImageButton back;
+    private ImageButton more;
+    private TextView title;
+
+    public ActionBarView(Context context) {
+        super(context);
+    }
+
+    public ActionBarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        View view = View.inflate(context, R.layout.layout_action_bar,null);
+        back = view.findViewById(R.id.back);
+        more = view.findViewById(R.id.more);
+        title = view.findViewById(R.id.title);
+
+        addView(view);
+    }
+
+    public void setOnBackClick(OnClickListener listener){
+        back.setOnClickListener(listener);
+    }
+
+    public void setOnMoreClick(OnClickListener listener){
+        more.setOnClickListener(listener);
+    }
+
+    public void setTitle(String s){
+        title.setText(s);
+    }
+}
