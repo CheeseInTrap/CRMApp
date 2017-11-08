dec741bdd5a140b1c93c54b55d329b1462e1cbc9
Zhujieru <chamber_of_secrets@163.com>
2017-11-09 00:21:18 +0800

    添加校园地图

--- app/src/main/java/com/fragment/SimpleMapFragment.java
+++ app/src/main/java/com/fragment/SimpleMapFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.user.crmapp.R;
-import com.view.ActionBarView;
 
 /**
  * 作者 ： Created by zjr on 2017/11/6 22:31.
@@ -14,7 +17,7 @@ import com.view.ActionBarView;
 
 public class SimpleMapFragment extends BaseFragment{
 
-    private ActionBarView actionBarView;
+    private WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,12 +29,36 @@ public class SimpleMapFragment extends BaseFragment{
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map_simple,container,false);
 
-
+        webView = (WebView) view.findViewById(R.id.web_view);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setSupportZoom(true);
+        settings.setBuiltInZoomControls(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl("http://map.hit.edu.cn/");
 
         return view;
     }
 
 
+
+    class MyWebChromeClient extends WebChromeClient{
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+
+        }
+    }
+
+    class MyWebViewClient extends WebViewClient{
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
 
 
 }
